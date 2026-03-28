2d5a2acd8c1d8106583750b410f04b73712a9bc8
Crain1211 <y252840@163.com>
2026-03-28 18:33:17 +0800

    fix: 恢复输出为静态网页 export 模式以适配 Cloudflare Pages

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   // 1. 将 "export" 修改为 "standalone"
-  output: "standalone",
+  output: "export",
   // basePath已被移除以适配 Cloudflare 根目录
   images: {
     unoptimized: true,
