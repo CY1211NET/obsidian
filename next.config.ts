b84ac0d01fa61fffa839a551698e6cc8456e427b
Crain1211 <y252840@163.com>
2026-03-28 18:26:50 +0800

    fix: remove basePath for CF deployment

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   // 1. 将 "export" 修改为 "standalone"
   output: "standalone",
-  basePath: isProd ? "/obsidian" : "",
+  // basePath已被移除以适配 Cloudflare 根目录
   images: {
     unoptimized: true,
   },
